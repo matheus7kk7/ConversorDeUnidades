a35ba2ca35e2b3911fc2df52f3bcd7402268cd96
matheus7kk7 <matheus7kifba@gmail.com>
2024-12-18 22:43:36 -0300

    Criação da base inicial do projeto ,Conversor dde Unidade de massas

--- main.c
+++ main.c
@@ -1,8 +1,115 @@
 #include <stdio.h>
 
+void menuPrincipal()
+{
+  printf(" 1 - Para conversor de massa \n");
+  printf(" 2 - Para conversor de comprimento \n");
+  printf(" 3 - Para conversor de volume \n");
+  printf(" 4 - Para conversor de temperatura \n");
+  printf(" 5 - Para conversor de tempo \n");
+  printf(" 6 - Para conversor de area \n");
+}
 
+void menuConversorDeMassa()
+{
+  int opcaoConversaoMassa;
+  float valorConversao, resultadoConversao;
+  do
+  {
+
+    printf("1 - Converter gramas em quilos \n");
+    printf("2 - Converter quilos em gramas \n");
+    printf("3 - Converter quilos em toneladas\n");
+    printf("0 - Voltar ao menu principal\n");
+    printf("Escolha uma opcao de conversao \n ");
+    scanf("%d", &opcaoConversaoMassa);
+    switch (opcaoConversaoMassa)
+    {
+    case 1:
+      printf("Informe o valor em gramas que serão convertido a quilos  \n");
+      scanf("%f", &valorConversao);
+      resultadoConversao = valorConversao / 1000;
+      printf(" %.2f gramas  = a %.2f quilos . \n", valorConversao, resultadoConversao);
+      break;
+    case 2:
+      printf("Informe o valor em quilos que serao convertidos a gramas  \n");
+      scanf("%f", &valorConversao);
+      resultadoConversao = valorConversao * 1000;
+      printf(" %.2f quilos e = a %.2f gramas . \n", valorConversao, resultadoConversao);
+      break;
+
+    case 3:
+      printf("Informe o valor em quilos que são convertidos a toneladas \n");
+      scanf("%f", &valorConversao);
+      resultadoConversao = valorConversao / 1000;
+      printf(" %.2f quilos e = a %.2f toneladas . \n", valorConversao, resultadoConversao);
+      break;
+
+    case 0:
+      printf("Voltando ao menu principal...\n");
+      break;
+    default:
+      printf("Opção inválida. Tente novamente.\n");
+      break;
+    }
+  } while (opcaoConversaoMassa != 0);
+}
+
+void menuConversorDeComprimento()
+{
+}
+void menuConversorDeVolume()
+{
+}
+void menuConversorDeTemperatura()
+{
+}
+
+void menuConversorDeTempo()
+{
+}
+void menuConversorDeArea()
+{
+}
 int main()
 {
 
-  
-  }
+  int opcao;
+
+  do
+  {
+
+    menuPrincipal();
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+      menuConversorDeMassa();
+      break;
+
+    case 2:
+      menuConversorDeComprimento();
+      break;
+
+    case 3:
+      menuConversorDeVolume();
+      break;
+    case 4:
+      menuConversorDeTemperatura();
+      break;
+    case 5:
+      menuConversorDeTempo();
+      break;
+    case 6:
+      menuConversorDeArea();
+      break;
+
+    default:
+      printf("opcao invalida \n ");
+      break;
+    }
+  } while (opcao != 0);
+  printf("saindo do programa \n");
+  return 0;
+}
